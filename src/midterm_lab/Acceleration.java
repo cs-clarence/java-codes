ed717bbfd223f03316e37e178a81e50869445182
Clarence Delmacio Manuel <rencedm112@gmail.com>
2021-12-02 10:21:46 +0800

    Midterms

---
+++ src/midterm_lab/Acceleration.java
@@ -0,0 +1,61 @@
+package midterm_lab;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.text.DecimalFormat;
+
+public class Acceleration {
+  public static void main(String[] args) throws IOException {
+    InputStreamReader isr = new InputStreamReader(System.in);
+    BufferedReader bf = new BufferedReader(isr);
+    System.out.print("Enter distance in meters: ");
+    double distance = Double.parseDouble(bf.readLine());
+    System.out.print("Enter the no. hours: ");
+    double hours = Double.parseDouble(bf.readLine());
+    System.out.print("Enter the no. minutes: ");
+    double minutes = Double.parseDouble(bf.readLine());
+    System.out.print("Enter the no. seconds: ");
+    double seconds = Double.parseDouble(bf.readLine());
+    printSpeed(distance, hours, minutes, seconds);
+  }
+
+  public static void printSpeed(double distanceInMeters, double hours, double minutes, double seconds) {
+    DecimalFormat df = new DecimalFormat("##########.##");
+    System.out.println("*********************************************");
+    System.out.println("Distance: " + df.format(distanceInMeters));
+    System.out.println("Hours: " + df.format(hours));
+    System.out.println("Minutes: " + df.format(minutes));
+    System.out.println("Seconds: " + df.format(seconds) + "\n");
+
+    System.out.println("Your speed in meters/second is "
+                       + df.format(distanceInMeters / toSeconds(
+      hours,
+      minutes,
+      seconds
+    )));
+
+    System.out.println("Your speed in km/h is "
+                       + df.format((distanceInMeters / 1000) / toHours(
+      hours,
+      minutes,
+      seconds
+    )));
+
+    System.out.println("Your speed in miles/h is "
+                       + df.format((distanceInMeters / 1609) / toHours(
+      hours,
+      minutes,
+      seconds
+    )));
+
+    System.out.println("*********************************************");
+  }
+
+  public static double toSeconds(double hours, double minutes, double seconds) {
+    return (hours * 3600) + (minutes * 60) + seconds;
+  }
+
+  public static double toHours(double hours, double minutes, double seconds) {
+    return hours + (minutes / 60) + (seconds / 3600);
+  }
+}
---
+++ src/midterm_lab/Change.java
@@ -0,0 +1,41 @@
+package midterm_lab;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class Change {
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    System.out.print("Enter change: ");
+    double change = in.nextDouble();
+
+    printModularizedChange(change);
+  }
+
+  public static void printModularizedChange(double change) {
+    double dollars = Math.floor(change / 100);
+    change -= dollars * 100;
+
+    double quarters = Math.floor(change / 25);
+    change -= quarters * 25;
+
+    double dimes = Math.floor(change / 10);
+    change -= dimes * 10;
+
+    double nickels = Math.floor(change / 5);
+    change -= nickels * 5;
+
+    DecimalFormat df = new DecimalFormat("#######.##");
+
+    System.out.println("Your change is: ");
+    System.out.println(df.format(dollars) + pluralize(" Dollar", dollars));
+    System.out.println(df.format(quarters) + pluralize(" Quarter", quarters));
+    System.out.println(df.format(dimes) + pluralize(" Dime", dimes));
+    System.out.println(df.format(nickels) + pluralize(" Nickel", nickels) + " and");
+    System.out.println(df.format(change) + pluralize(" Cent", change));
+  }
+
+  public static String pluralize(String s, Double n) {
+    return s + (n > 1 ? "s" : "");
+  }
+}
